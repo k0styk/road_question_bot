c52115f707fa2d622407c3bd9dfd6b81d3cef2b2
kostyk <rimshakonstantin@ya.ru>
2019-05-02 09:34:34 +0500

    meow

--- src/app.js
+++ src/app.js
@@ -5,6 +5,34 @@ const bot = new VkBot(config.getValue('token'));
 
 bot.command('/start', (ctx) => {
   ctx.reply('Hello!')
+  log("Start command");
 });
 
+bot.on((ctx) => {
+  ctx.reply("I want some command");
+  log("On event");
+})
+
 bot.startPolling();
+log("Bot started");
+
+function log(message) {
+  console.log(new Date().toLocaleTimeString()+": "+message);
+}
+
+// Context {
+//  message: 
+//   { date: 1556766687,
+//     from_id: 95123545,
+//     id: 9,
+//     out: 0,
+//     peer_id: 95123545,
+//     text: '/start',
+//     conversation_message_id: 7,
+//     fwd_messages: [],
+//     important: false,
+//     random_id: 0,
+//     attachments: [],
+//     is_hidden: false,
+//     type: 'message_new' },
+//
