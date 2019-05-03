7759f03abd9dbb293aa072ac34294b6fcad957d1
Kostyk <mr.proper-89@mail.ru>
2019-05-03 08:12:48 +0500

    New features

--- src/scenes/scenes.js
+++ src/scenes/scenes.js
@@ -1,20 +1,28 @@
 const VkBot = require('node-vk-bot-api/lib');
-const Session = require('node-vk-bot-api/lib/session');
 const Stage = require('node-vk-bot-api/lib/stage');
 const Scene = require('node-vk-bot-api/lib/scene');
+const Markup = require('node-vk-bot-api/lib/markup');
 
-// const scenes = [];
-
-class scenes {
+class Scenes {
   
   constructor() {
-    this.meet = new Scene('meet',
+    this.register = new Scene('register',
         (ctx) => {
           ctx.scene.next();
-          ctx.reply('How old are you?');
+          ctx.reply('Для регистрации нам необходимы данные о вас:\nВы являетесь учеником или сотрудником?', null, Markup
+          .keyboard(
+            [
+              Markup.button('Ученик','primary', {})
+            ],
+            [
+              Markup.button('Сотрудник','default', {})
+            ]
+          )
+          .oneTime());
         },
         (ctx) => {
-          ctx.session.age = +ctx.message.text;
+          console.log(ctx);
+          ctx.session.isStudent = +ctx.message.text;
   
           ctx.scene.next();
           ctx.reply('What is your name?');
@@ -29,30 +37,6 @@ class scenes {
     } 
 };
 
-// const session = new Session();
-// const scene = new Scene('meet',
-//   (ctx) => {
-//     ctx.scene.next();
-//     ctx.reply('How old are you?');
-//   },
-//   (ctx) => {
-//     ctx.session.age = +ctx.message.text;
+//^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$
 
-//     ctx.scene.next();
-//     ctx.reply('What is your name?');
-//   },
-//   (ctx) => {
-//     ctx.session.name = ctx.message.text;
-
-//     ctx.scene.leave();
-//     ctx.reply(`Nice to meet you, ${ctx.session.name} (${ctx.session.age} years old)`);
-//   });
-// const stage = new Stage(scene);
-
-// scenes.push({
-//   scene: scene,
-//   stage: stage,
-//   session: session
-// });
-
-module.exports = scenes;
+module.exports = Scenes;
